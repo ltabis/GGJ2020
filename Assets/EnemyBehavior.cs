a7346260fc6232f7b1787ce08b64ecfb5b3d83d9
matteovol <matteo.volpi@epitech.eu>
2020-01-31 23:22:15 +0100

    enemy go to player

---
+++ Assets/EnemyBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public float speed = 0.1f;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.Find("Square").transform;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed);
+    }
+}
