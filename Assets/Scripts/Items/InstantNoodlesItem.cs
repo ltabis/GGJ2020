933897ebb790645339402964a5cb63e4b3ea05ef
ltabis <lucas.tabis@epitech.eu>
2020-02-01 00:32:56 +0100

    :sparkles: AdDeD NooDlES

---
+++ Assets/Scripts/Items/InstantNoodlesItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantNoodlesItem : AItem, IItem
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        _description = "A basic set of noodles. Cheap. But strong has hell.";
+        _name = "Instant noodles";
+        _price = 100;
+        _rarity = Rarity.Epic;
+    }
+
+    public void Use()
+    {
+        Debug.Log("The noodles are beeing used!");
+    }
+}
