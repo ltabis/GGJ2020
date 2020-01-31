a1c1f82a4ca96cd39d1d023b98787daf154209de
Arthur <arthur.lang@epitech.eu>
2020-01-31 23:53:28 +0100

    add ARepairable and bullet prefab (with animation)

---
+++ Assets/Scripts/AReperable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AReperable : MonoBehaviour
+{
+    public enum ReperableStatus
+    {
+        Broken, Repair, Use
+    };
+
+    public float repairTime;
+    public ReperableStatus status = ReperableStatus.Broken;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Repair(float value)
+    {
+        if (status == ReperableStatus.Broken)
+            status = ReperableStatus.Use;
+        repairTime -= value;
+        if (repairTime <= 0)
+            status = ReperableStatus.Repair;
+    }
+}
--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReperableBehaviour : MonoBehaviour
+public class BulletBehaviour : MonoBehaviour
 {
+
+    public Vector3 direction = new Vector3(1, 0, 0);
+    public float speed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(direction * speed * Time.deltaTime, 0);
     }
 }
