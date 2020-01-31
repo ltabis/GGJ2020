8e0e1346271b19f08cd53dbce6d9a3e38fb099a0
ltabis <lucas.tabis@epitech.eu>
2020-01-31 22:45:47 +0100

    :sparkles: Added abstract class for all entities + Added a basic mouse tracking for the player.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : AEntity
+{
+    // Update is called once per frame
+    void Update()
+    {
+        RotateEntityWithMouse();
+        Movements();
+    }
+
+    private void Movements()
+    {
+
+    }
+
+    // Rotate the object, following the mouse cursor.
+    private void RotateEntityWithMouse()
+    {
+        // Transform position into screen position.
+        var pos = Camera.main.WorldToScreenPoint(transform.position);
+
+        // Getting distance between mouse and object.
+        var dir = Input.mousePosition - pos;
+
+        // Weird maths 0_0
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+
+        // Applying rotation to the object.
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
