bddc67fb0954451ffa8fb1dec2a1617401e67379
ltabis <lucas.tabis@epitech.eu>
2020-02-01 12:07:22 +0100

    :sparkles: Started inventory.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class AInventory : MonoBehaviour
+public class Inventory : MonoBehaviour
 {
+    // UI.
+    public Canvas canvas;
+
     // Public.
     public List<Item> prefabList;
 
     // Private.
-    protected string _name;
-    protected uint _fixedSize;
+    protected string _name = "Inventory";
+    protected uint _fixedSize = 0;
     protected List<Item> _content;
+
+    private void Start()
+    {
+        Generate();
+    }
+
+    // Set the inventory visible or not.
+    public void ToggleInventory(bool toggle)
+    {
+        canvas.gameObject.SetActive(toggle);
+    }
 
     // Take an item from the current inventory
     public Item Take(string toFind)
@@ -21,13 +35,35 @@ public abstract class AInventory : MonoBehaviour
         {
             // Found the item, getting it.
             if (item.name == toFind)
-                return item;
-
-            _content.Remove(item);
+            {
+                // Removing the item from the inventory list.
+                var itemCpy = item;
+                _content.Remove(item);
+                return itemCpy;
+            }
         }
 
         // Item hasn't been found.
         return null;
+    }
+
+    // Removes item from the current inventory
+    public void Remove(string toFind)
+    {
+
+        // Searching for the item.
+        foreach (var item in _content)
+        {
+            // Found the item, getting it.
+            if (item.name == toFind)
+                _content.Remove(item);
+        }
+    }
+
+    // Adds an item to the inventory.
+    public void Add(Item item)
+    {
+        _content.Add(item);
     }
 
     // Generates a random set of items.
