a424a6bda62873485381a6b9e0a380e54a446278
thomaslopvet <42534385+thomaslopvet@users.noreply.github.com>
2020-02-01 20:54:27 +0100

    Sounds and titlescreen

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour
+{
+    public string sceneName;
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(LoadStartScene);
+    }
+
+    private void LoadStartScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
